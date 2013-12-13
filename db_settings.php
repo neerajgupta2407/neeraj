2900b8962581a3071aa9a0f1fa85d15b4b4c5c23
neerajgupta2407 <neerajgupta2407@gmail.com>
2013-12-13 21:52:24 +0530

    Update db_settings.php

--- db_settings.php
+++ db_settings.php
@@ -4,28 +4,12 @@ include_once('../config.php');
 //echo error_log("You messed up! \r\n", 3, SITE."logs/db_error.txt");
 
 $db_config = array();
-/*Test */
-/*
-$db_config['main_db'] =array("host" => "localhost",
-							 "user" => "dineout_test",
-							 "pass" => "Dine@test449",
-							 "db_name" => "dineout_test"	
-							 );
-*/
-/*Live*/
+
 $db_config['main_db'] =array("host" => "localhost",
 							 "user" => "root",
 							 "pass" => "",
 							 "db_name" => "dl"	
 							 );
 
-/*
-$db_config['main_db'] =array("host" => "localhost",
-							 "user" => "root",
-							 "pass" => "",
-							 "db_name" => "dineout_live"	
-							 );
-*/
-//echo $db_config['main_db']['host'];
 
 ?>
